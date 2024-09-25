87eaf0c76d7832cb7a709072b796e8bfca6281ca
GiangchauVo <giangchau11091980@gmail.com>
2024-09-25 20:29:38 +0700

    Mới tạo code

---
+++ Bai05Tinhdientichhinhtamgiac/src/baithuchanh/bai05/Tinhdientichhinhtamgiac.java
@@ -0,0 +1,41 @@
+package baithuchanh.bai05;
+import java.util.Scanner;
+public class Tinhdientichhinhtamgiac {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		float p, s, cv;
+		a=NhapSoNguyen("Nhap canh a : ");
+		b=NhapSoNguyen("Nhap canh b : ");
+		c=NhapSoNguyen("Nhap canh c : ");
+		p=nuaChuvi(a,b,c);
+		s=dienTich(a,b,c);
+		cv=Chuvi(a,b,c);
+		System.out.println("Nua chu vi hinh tam giac la :"+p);
+		System.out.println("Chu vi hinh tam giac la :"+cv);
+		System.out.println("Dien tich hinh tam giac la :"+s);
+		
+		
+	}
+
+	public static float Chuvi(int a, int b, int c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static float dienTich(int a, int b, int c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static float nuaChuvi(int a, int b, int c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static int NhapSoNguyen(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
