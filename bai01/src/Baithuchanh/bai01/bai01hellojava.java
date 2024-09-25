afb10fb9966964a58cfba03218c2334eda72bd5b
GiangchauVo <giangchau11091980@gmail.com>
2024-09-25 19:15:29 +0700

    Đã tạo lại Bai01

---
+++ bai01/src/Baithuchanh/bai01/bai01hellojava.java
@@ -0,0 +1,18 @@
+package Baithuchanh.bai01;
+
+public class bai01hellojava {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 7;
+		int c=a+b;
+		System.out.println("HÔM NAY TÔI BẮT ĐẦU HỌC JAVA");
+		System.out.println("xin chào Java");
+		System.out.println("Tổng là: + c");
+	}
+
+
+	}
+
+
