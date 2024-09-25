371173e3d992d6af04309e814c8cf8e5291a1d73
GiangchauVo <giangchau11091980@gmail.com>
2024-09-25 19:27:53 +0700

    Mới làm lại bài tập ngày 25 tháng 9

---
+++ Bai02/src/baithuchanh/bai02/Bai02HelloJava.java
@@ -0,0 +1,14 @@
+package baithuchanh.bai02;
+
+public class Bai02HelloJava {
+
+	public static void main(String[] args) {
+		int a = 1000000000;
+		int b = 1000000000;
+		int c = a+ b;
+		System.out.println("Xin chào Java");
+		System.out.println("Hôm nay là buổi thứ 2 tôi học Java");
+		System.out.println("Tổng tiền tôi có được là: "+ c);
+	}
+
+}
--- bai01/src/Baithuchanh/bai01/bai01hellojava.java
+++ bai01/src/Baithuchanh/bai01/bai01hellojava.java
@@ -6,10 +6,10 @@ public class bai01hellojava {
 		
 		int a = 5;
 		int b = 7;
-		int c=a+b;
+		int c = a+ b;
 		System.out.println("HÔM NAY TÔI BẮT ĐẦU HỌC JAVA");
-		System.out.println("xin chào Java");
-		System.out.println("Tổng là: + c");
+		System.out.println("Xin chào Java");
+		System.out.println("Tổng là:" + c);
 	}
 
 
