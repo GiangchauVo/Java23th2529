0d524d8e8dcc02f8a6cef7039a61935f9516ef05
GiangchauVo <giangchau11091980@gmail.com>
2024-09-26 22:22:27 +0700

    Vừa tạo code

---
+++ Lab1bai1/src/baithuchanh/lab1bai1/Lab1bai1.java
@@ -0,0 +1,14 @@
+package baithuchanh.lab1bai1;
+import java.util.Scanner;
+public class Lab1bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Họ và tên: ");
+		String hoTen = scanner.nextLine();
+		System.out.print("Điểm TB: ");
+		double diemTB = scanner.nextDouble();
+
+	}
+
+}
